fc1d743051524c24fba4340d95d1da8419b4f2c9
DrZiMo <zuhaybhamar@gmail.com>
2025-04-30 18:03:23 +0300

    added the data

---
+++ data.js
@@ -0,0 +1,82 @@
+const images = [
+    {
+        image: 'https://picsum.photos/200/300?random=1',
+        age: 34,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=2',
+        age: 72,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=3',
+        age: 15,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=4',
+        age: 89,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=5',
+        age: 5,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=6',
+        age: 43,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=7',
+        age: 56,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=8',
+        age: 23,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=9',
+        age: 67,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=10',
+        age: 91,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=11',
+        age: 12,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=12',
+        age: 28,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=13',
+        age: 50,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=14',
+        age: 61,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=15',
+        age: 3,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=16',
+        age: 45,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=17',
+        age: 82,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=18',
+        age: 19,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=19',
+        age: 77,
+    },
+    {
+        image: 'https://picsum.photos/200/300?random=20',
+        age: 30,
+    },
+]
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const age = document.querySelector('.age')
 const slider = document.querySelector('.slider')
 const currentRoundNum = document.querySelector('.round-number')
 const totalRoundNum = document.querySelector('.total-round')
+const mainImage = document.querySelector('.image')
 
 let currentRound = 1
 let numberOfRounds = 10
@@ -9,9 +10,16 @@ let numberOfRounds = 10
 window.addEventListener('load', () => {
     currentRoundNum.innerHTML = currentRound
     totalRoundNum.innerHTML = numberOfRounds
+    getRandomImage()
 })
 
 slider.addEventListener('input', (e) => {
     age.innerHTML = e.target.value
-    console.log(e)
 })
+
+const getRandomImage = () => {
+    const randomNumber = Math.floor(Math.random() * images.length - 1)
+    const image = images[randomNumber].image
+
+    mainImage.src = image
+}
